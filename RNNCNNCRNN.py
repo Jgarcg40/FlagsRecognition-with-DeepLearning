7edea32470a4f86ec7f43f724efeb1d5dc9c29a8
Jgarcg40 <Jgarcg40@estudiantes.unileon.es>
2023-04-05 15:46:18 +0200

    Programas finalizados

--- RNNCNNCRNN.py
+++ RNNCNNCRNN.py
@@ -2,6 +2,8 @@
 import glob
 import os
 import random
+from tkinter import filedialog
+from tkinter import *
 
 import numpy as np
 import tensorflow as tf
@@ -15,7 +17,7 @@ from tensorflow.keras.preprocessing import image  # type: ignore
 # # Semilla y directorios
 
 
-seed_value = 42
+seed_value = 42   #semilla para que no varien los resultados en cada ejecucion
 random.seed(seed_value)
 np.random.seed(seed_value)
 tf.random.set_seed(seed_value)
@@ -23,27 +25,26 @@ tf.random.set_seed(seed_value)
 os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK']='True' #para evitar mensajes  de error
 
-train_dir = r'C:\Users\yoni8\Desktop\dataset_train'
-validation_dir = r'C:\Users\yoni8\Desktop\dataset_validation'
-test_dir = r'C:\Users\yoni8\Desktop\dataset_test'
+train_dir = 'resources\dataset_train'
+validation_dir = 'resources\dataset_validation'  #directorios con los datasets
+test_dir = 'resources\dataset_test'
 
 # # Etiquetado de clases
-
 
 train_datagen = ImageDataGenerator(rescale=1. / 255)
 validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
 directories = sorted(glob.glob(os.path.join(train_dir, '*')))
-labels = [os.path.basename(d) for d in directories]
+labels = [os.path.basename(d) for d in directories] #mostramos los nombres de la carpeta train_dir por orden
 print(labels)
 
 train_labels = []
 for d in directories:
-    label = os.path.basename(d)
+    label = os.path.basename(d)   #obtenemos los nombres de la carpeta train_dir
     files = glob.glob(os.path.join(d, '*'))
-    train_labels += [labels.index(label)] * len(files)
+    train_labels += [labels.index(label)] * len(files) #etiquetamos cada foto de cada clase y metemos las etiquetas en train_labels
 
 #print(train_labels)
 
@@ -52,9 +53,9 @@ for d in directories:
 train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(124, 124),
-    batch_size=20,
-    class_mode='categorical',
-    shuffle=True,
+    batch_size=20, #tamaño lote
+    class_mode='categorical', #porque hay muchas clases
+    shuffle=True, #mezclar aleatoriamente los datos
     seed=42)
 
 validation_generator = validation_datagen.flow_from_directory(
@@ -65,33 +66,34 @@ validation_generator = validation_datagen.flow_from_directory(
     shuffle=False)
 
 # # Parámetros entrenamiento
+
 # Definir parámetros de la CNN
 CNN_model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(124, 124, 3)),
-    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(124, 124, 3)), #capa convolucional
+    layers.MaxPooling2D((2, 2)), #reduce dimension espacial
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(128, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(128, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
-    layers.Flatten(),
-    layers.Dense(512, activation='relu'),
-    layers.Dense(len(labels), activation='softmax')
+    layers.Flatten(), #capa aplanamiento
+    layers.Dense(512, activation='relu'), #capa densa con 512 neuronas
+    layers.Dense(len(labels), activation='softmax') #producimos las probavilidades
 ])
 
 # Definir parámetros de la RNN
 
 RNN_model = models.Sequential([
-    layers.Reshape((1, 124, 124, 3), input_shape=(124, 124, 3)),
-    layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu')),
+    layers.Reshape((1, 124, 124, 3), input_shape=(124, 124, 3)), #remodelar a 4 dimensiones
+    layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu')), #usamos secuencias temporales
     layers.TimeDistributed(layers.MaxPooling2D((2, 2))),
     layers.TimeDistributed(layers.Conv2D(64, (3, 3), activation='relu')),
     layers.TimeDistributed(layers.MaxPooling2D((2, 2))),
     layers.TimeDistributed(layers.Conv2D(128, (3, 3), activation='relu')),
     layers.TimeDistributed(layers.MaxPooling2D((2, 2))),
     layers.TimeDistributed(layers.Flatten()),
-    layers.LSTM(256),
+    layers.LSTM(256),  #256 unidades LSTM
     layers.Dense(len(labels), activation='softmax')
 ])
 
@@ -104,7 +106,7 @@ CRNN_model = models.Sequential([
     layers.MaxPooling2D((2, 2)),
     layers.TimeDistributed(layers.Flatten()),
     layers.LSTM(256, return_sequences=True),
-    layers.GlobalMaxPooling1D(),
+    layers.GlobalMaxPooling1D(),  # reduce la dimensión de la salida de la capa LSTM seleccionando el valor máximo de cada canal de características
     layers.Dense(len(labels), activation='softmax')
 ])
 
@@ -127,9 +129,9 @@ model_trained_RNN = False
 model_trained_CRNN = False
 # Cargar modelo CNN ya entrenado
 try:
-    CNN_model = models.load_model(r'C:\Users\yoni8\Desktop\saved_model_CNN.h5')
+    CNN_model = models.load_model('resources\saved_model_CNN.h5')
     print("Modelo CNN cargado")
-    history_CNN = CNN_model.history
+    history_CNN = CNN_model.history #guardar estadisticas
     model_trained_CNN = True
 
 except:
@@ -137,7 +139,7 @@ except:
 
 # Cargar modelo RNN ya entrenado
 try:
-    RNN_model = models.load_model(r'C:\Users\yoni8\Desktop\saved_model_RNN.h5')
+    RNN_model = models.load_model('resources\saved_model_RNN.h5')
     print("Modelo RNN cargado")
     history_RNN =RNN_model.history
     model_trained_RNN = True
@@ -147,7 +149,7 @@ except:
 
 # Cargar modelo CRNN ya entrenado
 try:
-    CRNN_model = models.load_model(r'C:\Users\yoni8\Desktop\saved_model_CRNN.h5')
+    CRNN_model = models.load_model('resources\saved_model_CRNN.h5')
     print("Modelo CRNN cargado")
     history_RNN =CRNN_model.history
     model_trained_CRNN = True
@@ -168,14 +170,14 @@ while True:
 
         # Entrenar modelo CNN
         print("Entrenando CNN")
-        early_stop = callbacks.EarlyStopping(monitor='val_accuracy', patience=50, restore_best_weights=True)
+        early_stop = callbacks.EarlyStopping(monitor='val_accuracy', patience=50, restore_best_weights=True) #con esto para cuando el modelo no mejora
         history_CNN = CNN_model.fit(train_generator,
-                                steps_per_epoch=150,
-                                epochs=200,
+                                steps_per_epoch=150,  #pasos entrenamiento
+                                epochs=200,   #iteraciones
                                 validation_data=validation_generator,
                                 validation_steps=150,
                                 callbacks=[early_stop])
-        CNN_model.save(r'C:\Users\yoni8\Desktop\saved_model_CNN.h5', include_optimizer=True)
+        CNN_model.save('resources\saved_model_CNN.h5', include_optimizer=True) #guarda entrenamiento
         model_trained_CNN = True
         # Evaluar modelo CNN
         test_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -206,8 +208,9 @@ while True:
       if model_trained_CNN:
 
         # Predicción
-
-        img_path = r'C:\Users\yoni8\Desktop\españa.jpg'
+        root = Tk()
+        root.withdraw()
+        img_path = filedialog.askopenfilename()
         img = image.load_img(img_path, target_size=(124, 124))
         img_tensor = image.img_to_array(img)
         img_tensor = np.expand_dims(img_tensor, axis=0)
@@ -215,14 +218,14 @@ while True:
 
         predictions = CNN_model.predict(img_tensor)
         predicted_label_idx = np.argmax(predictions)
-        predicted_label = labels[predicted_label_idx]
-        print('Predicted label:', predicted_label)
+        predicted_label = labels[predicted_label_idx] #muestra la prediccion con su etiqueta
+        print('Pais resultado:', predicted_label)
 
         sorted_idxs = np.argsort(predictions[0])[::-1]
         for idx in sorted_idxs:
             label = labels[idx]
             prob = predictions[0][idx]
-            #print('{}: {:.4%}'.format(label, prob))
+            print('{}: {:.4%}'.format(label, prob)) #toda esta parte con el for para ver las provabilidades resto paises
 
       Error = True
 
@@ -238,7 +241,7 @@ while True:
                                         validation_data=validation_generator,
                                         validation_steps=150,
                                         callbacks=[early_stop2])
-            RNN_model.save(r'C:\Users\yoni8\Desktop\saved_model_RNN.h5')
+            RNN_model.save('resources\saved_model_RNN.h5',include_optimizer=True)
             model_trained_RNN = True
 
             # Evaluar modelo CNN
@@ -269,7 +272,9 @@ while True:
         if model_trained_RNN:
             # Predicción
 
-            img_path = r'C:\Users\yoni8\Desktop\españa.jpg'
+            root = Tk()
+            root.withdraw()
+            img_path = filedialog.askopenfilename()
             img = image.load_img(img_path, target_size=(124, 124))
             img_tensor = image.img_to_array(img)
             img_tensor = np.expand_dims(img_tensor, axis=0)
@@ -278,13 +283,13 @@ while True:
             predictions = RNN_model.predict(img_tensor)
             predicted_label_idx = np.argmax(predictions)
             predicted_label = labels[predicted_label_idx]
-            print('Predicted label:', predicted_label)
+            print('Pais resultado:', predicted_label)
 
             sorted_idxs = np.argsort(predictions[0])[::-1]
             for idx in sorted_idxs:
                 label = labels[idx]
                 prob = predictions[0][idx]
-                # print('{}: {:.4%}'.format(label, prob))
+                print('{}: {:.4%}'.format(label, prob))
 
         Error = True
 
@@ -300,7 +305,7 @@ while True:
                                         validation_data=validation_generator,
                                         validation_steps=150,
                                         callbacks=[early_stop3])
-            RNN_model.save(r'C:\Users\yoni8\Desktop\saved_model_CRNN.h5')
+            CRNN_model.save('resources\saved_model_CRNN.h5',include_optimizer=True)
             model_trained_CRNN = True
 
             # Evaluar modelo CNN
@@ -331,7 +336,9 @@ while True:
         if model_trained_CRNN:
             # Predicción
 
-            img_path = r'C:\Users\yoni8\Desktop\españa.jpg'
+            root = Tk()
+            root.withdraw()
+            img_path = filedialog.askopenfilename()
             img = image.load_img(img_path, target_size=(124, 124))
             img_tensor = image.img_to_array(img)
             img_tensor = np.expand_dims(img_tensor, axis=0)
@@ -340,23 +347,24 @@ while True:
             predictions = CRNN_model.predict(img_tensor)
             predicted_label_idx = np.argmax(predictions)
             predicted_label = labels[predicted_label_idx]
-            print('Predicted label:', predicted_label)
+            print('Pais resultado:', predicted_label)
 
             sorted_idxs = np.argsort(predictions[0])[::-1]
             for idx in sorted_idxs:
                 label = labels[idx]
                 prob = predictions[0][idx]
-                # print('{}: {:.4%}'.format(label, prob))
+                print('{}: {:.4%}'.format(label, prob))
 
         Error = True
 
     if not Error:
 
-        print("Error, ingrese 1 para CNN o 2 para RNN.")
+        print("Error, ingrese 1 para CNN, 2 para RNN o 3 para CRNN.")
 
     else:
 
-        print("¿Quieres salir de la aplicación? Escribe 'no' y si quieres permanecer toca letra o numero:")
+        print("¿Quieres salir de la aplicación? Escribe 'no' y si quieres permanecer toca una letra o numero cualquiera:")
         respuesta = input()
         if respuesta.lower() == 'no':
             break
+#end
---
+++ inter3.py
@@ -0,0 +1,246 @@
+# # Imports
+import contextlib
+import glob
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import ImageTk, Image
+import numpy as np
+from keras import models
+from tensorflow.keras.preprocessing import image  # type: ignore
+import wikipedia
+import re
+import tensorflow as tf
+#pyinstaller --onefile --add-data "resources;resources" --icon="resources/mundo.ico" --name "Interpretador de banderas" -w inter3.py
+
+
+# se necesita hacer pip install wikipedia
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+#ventana principal
+
+root = tk.Tk()
+root.geometry('800x600')
+root.title("Banderas")
+root.configure(bg='#FFFFFF')
+icon_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'mundo.ico'))
+
+# Define el icono de la ventana
+root.iconbitmap(default=icon_path)#cambia el icono de la barra de herramientas
+
+# Cargar imagen de fondo
+image_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'banderas2.png'))
+img = Image.open(image_path)
+img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight()), Image.LANCZOS)
+bg = ImageTk.PhotoImage(img)
+
+# Cargar imagen de fondo de los botnes
+cnn_image = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'cielo.jpg'))
+
+cnn_im = Image.open(cnn_image)
+cnn_im = cnn_im.resize((root.winfo_screenwidth(), root.winfo_screenheight()), Image.LANCZOS)
+cnn_bg = ImageTk.PhotoImage(cnn_im)
+
+# Crear canvas
+canvas = tk.Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+canvas.pack()
+
+
+# Mostrar imagen de fondo ventana principal
+canvas.create_image(0, 0, anchor=tk.NW, image=bg)
+
+
+
+
+dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'dataset_test'))
+
+# # Etiquetado de clases
+
+directories = sorted(glob.glob(os.path.join(dir, '*')))
+labels = [os.path.basename(d) for d in directories] #mostramos los nombres de la carpeta train_dir por orden
+
+train_labels = []
+for d in directories:
+    label = os.path.basename(d)   #obtenemos los nombres de la carpeta train_dir
+    files = glob.glob(os.path.join(d, '*'))
+    train_labels += [labels.index(label)] * len(files) #etiquetamos cada foto de cada clase y metemos las etiquetas en train_labels
+
+
+#final codigo entrenamiento
+# Función para abrir y mostrar imagen seleccionada
+img_path = ""
+def open_image():
+    global img_path
+    img_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp")])#abre imágenes de este tipo solo
+    messagebox.showinfo(title="Imagen cargada", message="Has cargado la imagen correctamente")
+    img = image.load_img(img_path, target_size=(124, 124))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+    return img_tensor
+
+
+
+def get_summary(country): #funcion para texto de wikipedia
+    wikipedia.set_lang("es")
+    page = wikipedia.page(country)
+    raw_summary = page.summary
+    summary = re.sub(r'\[\d+\]', '', raw_summary)#eliminar simbolos
+    summary_lines = summary.split('\n')[:2]#resumen
+    summary = '\n'.join(summary_lines)
+    return summary
+
+
+# Función para ejecutar código del botón CNN
+def cnn_function():
+    dir2 = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'saved_model_CNN.h5'))
+    global img_path
+    if not img_path:
+        messagebox.showerror(title="Error", message="Debes introducir una imagen antes")
+        return
+    CNN_model = models.load_model(dir2)
+    img = image.load_img(img_path, target_size=(124, 124))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+    null_out = open(os.devnull, 'w')
+    with contextlib.redirect_stdout(null_out):
+        predictions = CNN_model.predict(img_tensor)
+        predicted_label_idx = np.argmax(predictions)
+        predicted_label = labels[predicted_label_idx]
+
+    null_out.close()
+
+
+    summary = get_summary(predicted_label) #wikipedia
+
+    # Ventana
+    cnn_window = tk.Toplevel(root)
+    cnn_window.title("Resultado CNN")
+    cnn_window.geometry('800x600')
+    cnn_window.configure(bg='#99CCFF')
+
+
+    canvas_crnn = tk.Canvas(cnn_window, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+    canvas_crnn.place(x=0, y=0)
+
+
+    canvas_crnn.create_image(0, 0, anchor=tk.NW, image=cnn_bg)
+
+
+    img = Image.open(img_path)
+    img = img.resize((500, 500), Image.LANCZOS)
+    img = ImageTk.PhotoImage(img)
+    panel = tk.Label(cnn_window, image=img, bg='#99CCFF')
+    panel.image = img
+    panel.pack(side="top", pady=10)
+
+    # predicción
+    label = tk.Label(cnn_window, text=f"Bandera de {predicted_label}", font=("Arial", 20), bg='#FFFFFF')
+    label.pack(side="top", pady=10)
+
+    # wikipedia texto
+    summary_label = tk.Label(cnn_window, text=summary, font=("Arial", 12), bg='#FFFFFF', wraplength=700,
+                             justify=tk.LEFT)
+    summary_label.pack(side="top", pady=10)
+
+
+# Función para ejecutar código del botón RNN
+def rnn_function():
+    dir3 = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'saved_model_RNN.h5'))
+
+    global img_path
+    if not img_path:
+        messagebox.showerror(title="Error", message="Debes introducir una imagen antes")
+        return
+    RNN_model = models.load_model(dir3)
+    img = image.load_img(img_path, target_size=(124, 124))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+    null_out = open(os.devnull, 'w')
+    with contextlib.redirect_stdout(null_out):
+        predictions = RNN_model.predict(img_tensor)
+        predicted_label_idx = np.argmax(predictions)
+        predicted_label = labels[predicted_label_idx]
+
+    null_out.close()
+    summary = get_summary(predicted_label)
+    rnn_window = tk.Toplevel(root)
+    rnn_window.title("Resultado RNN")
+    rnn_window.geometry('800x600')
+    rnn_window.configure(bg='#99CCFF')
+    canvas_crnn = tk.Canvas(rnn_window, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+    canvas_crnn.place(x=0, y=0)
+    canvas_crnn.create_image(0, 0, anchor=tk.NW, image=cnn_bg)
+    img = Image.open(img_path)
+    img = img.resize((500, 500), Image.LANCZOS)
+    img = ImageTk.PhotoImage(img)
+    panel = tk.Label(rnn_window, image=img, bg='#99CCFF')
+    panel.image = img
+    panel.pack(side="top", pady=10)
+    label = tk.Label(rnn_window, text=f"Bandera de {predicted_label}", font=("Arial", 20), bg='#FFFFFF')
+    label.pack(side="top", pady=10)
+    summary_label = tk.Label(rnn_window, text=summary, font=("Arial", 12), bg='#FFFFFF', wraplength=700,justify=tk.LEFT)
+    summary_label.pack(side="top", pady=10)
+
+# Función para ejecutar código del botón CRNN
+
+def crnn_function():
+    dir4 = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources', 'saved_model_CRNN.h5'))
+    global img_path
+    if not img_path:
+        messagebox.showerror(title="Error", message="Debes introducir una imagen antes")
+        return
+
+    CRNN_model = models.load_model(dir4)
+    img = image.load_img(img_path, target_size=(124, 124))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+    null_out = open(os.devnull, 'w')
+    with contextlib.redirect_stdout(null_out):
+        predictions = CRNN_model.predict(img_tensor)
+        predicted_label_idx = np.argmax(predictions)
+        predicted_label = labels[predicted_label_idx]
+
+    null_out.close()
+    summary = get_summary(predicted_label)
+    crnn_window = tk.Toplevel(root)
+    crnn_window.title("Resultado CRNN")
+    crnn_window.geometry('800x600')
+    crnn_window.configure(bg='#99CCFF')
+    canvas_crnn = tk.Canvas(crnn_window, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+    canvas_crnn.place(x=0, y=0)
+    canvas_crnn.create_image(0, 0, anchor=tk.NW, image=cnn_bg)
+    img = Image.open(img_path)
+    img = img.resize((500, 500), Image.LANCZOS)
+    img = ImageTk.PhotoImage(img)
+    panel = tk.Label(crnn_window, image=img, bg='#99CCFF')
+    panel.image = img
+    panel.pack(side="top", pady=10)
+    label = tk.Label(crnn_window, text=f"Bandera de {predicted_label}", font=("Arial", 20), bg='#FFFFFF')
+    label.pack(side="top", pady=10)
+    summary_label = tk.Label(crnn_window, text=summary, font=("Arial", 12), bg='#FFFFFF', wraplength=700, justify=tk.LEFT)
+    summary_label.pack(side="top", pady=10)
+
+
+
+# boton para meter la imagen
+btn_open = tk.Button(root, text="Introducir imagen", font=("Arial", 16), bg="#009933", fg="black", command=open_image)
+btn_open.place(relx=0, rely=0.75, relwidth=1, relheight=0.15)
+
+# botones redes neuronales
+btn_cnn = tk.Button(root, text="CNN", font=("Arial", 16), bg="#66CCFF", fg="black",command=cnn_function)
+btn_cnn.place(relx=0, rely=0.9, relwidth=0.333, relheight=0.1)
+
+btn_rnn = tk.Button(root, text="RNN", font=("Arial", 16), bg="#E63D3D", fg="black",command=rnn_function)
+btn_rnn.place(relx=0.333, rely=0.9, relwidth=0.333, relheight=0.1)
+
+btn_crnn = tk.Button(root, text="CRNN", font=("Arial", 16), bg="#F7FF00", fg="black",command=crnn_function)
+btn_crnn.place(relx=0.666, rely=0.9, relwidth=0.334, relheight=0.1)
+
+root.mainloop()
